686ac335683a0096a0360329e0520a17a4933adf
abujwed <abubakar.jawed@gmail.com>
2021-06-09 10:07:49 +0530

    update

---
+++ myfirst.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void prime(n)
+{
+    int i,r,s=0;
+    for(i=0;i<=n;i++)
+    {
+        r=n%i;
+        if(r==0)
+        {
+            s++;
+        }
+    }
+    if(s==2)
+    {
+        printf("%d\n",n);
+    }
+}
+
+int main()
+{
+    int n1,n2,i;
+    printf("Enter 1st number: ");
+    scanf("%d",&n1);
+    printf("Enter 2nd number: ");
+    scanf("%d",&n2);
+    for (i = n1; i <= n2; i++)
+    {
+        prime(i);
+    }
+    
+}
