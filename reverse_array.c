4bd445a4f7c83c1c406ca7e6be1e978bd73d67b9
abujawed11 <abubakar.jawed@gmail.com>
2021-06-14 19:14:08 +0530

    14_06

--- reverse_array.c
+++ reverse_array.c
@@ -1,35 +1,34 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 int printarr(int s[])
 {
-    
 
-    
-    
     // int lentgh = sizeof(s)/sizeof(s[0]);
     // printf("%d",lentgh);
     for (int i = 0; i < 9; i++)
     {
-        printf("%d\t",s[i]);
+        printf("%d\t", s[i]);
     }
 }
 
 int arrrever(int *ptr)
 {
-    //int lentgh = sizeof(*ptr)/sizeof(int);
+    int temp;
+    //printf("%d",*(ptr+8));
+    int lentgh = sizeof(*ptr) / sizeof(int);
     int a[20];
     for (int i = 0; i < 9; i++)
     {
-        a[i] = *(ptr+(8-i));
+        a[i] = *(ptr + (8 - i));
     }
     //printarr(a);
     for (int i = 0; i < 9; i++)
     {
-        *(ptr+i) = a[i];
+        *(ptr + i) = a[i];
     }
     //printf("%d",sizeof(a));
-    
+
     //printf("%d",*ptr);
     // *ptr = *(ptr+8);
     // *(ptr+1) = *(ptr+7);
@@ -37,12 +36,11 @@ int arrrever(int *ptr)
     //printf("%d",*(ptr+8));
 }
 
-
 int main()
 {
-    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     //int lentgh = sizeof(arr)/sizeof(arr[0]);
-    // while (arr[i] != )      
+    // while (arr[i] != )
     // {
     //     i++;
     // }
@@ -54,8 +52,6 @@ int main()
     printarr(arr);
     //printf("%d",strlen(arr));
     // arrrever(arr);
-
-
 
     return 0;
 }
